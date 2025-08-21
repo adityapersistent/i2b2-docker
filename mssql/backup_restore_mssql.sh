3e10c194c3b20ac1e5aceef7a2c63f7ca6109e34
Aditya vidap <aditya_vidap@persistent.com>
2025-08-21 15:55:44 +0530

    updated backup_restore for mssql

--- mssql/backup_restore_mssql.sh
+++ mssql/backup_restore_mssql.sh
@@ -1,74 +1,126 @@
-# sh backup_restore_mssql.sh dummy_host 1432 SA '<YourStrong@Passw0rd>' i2b2demodata i2b2metadata i2b2pm i2b2hive i2b2workdata
-# docker rm -f $(docker ps -a -q) #cleaning exising docker containers
-docker compose up -d
- 
-# we are restoring the database to a new docker mssql database container hence we are providing the docker network ip as a remote host ip
-docker_network_gateway_ip=$(docker network inspect i2b2-net -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
- 
-host=$1
+docker rm -f $(docker ps -a -q)
+docker-compose up -d 
 
-export port=$2
-export username=$3
-export password=$4
- 
-export crc_dbname=$5
-export ont_dbname=$6
-export pm_dbname=$7
-export hive_dbname=$8
-export wd_dbname=$9
- 
-echo "Host- $host Port- $port Username- $username Password- $password "
-echo "waiting for database docker container to get start"
- 
-#export host=$docker_network_gateway_ip
-#docker run -i -e "ACCEPT_EULA=Y"  -e "SA_PASSWORD=<YourStrong@Passw0rd>"  -p 1432:1433 --net i2b2-net -v i2b2-mssql-vol-gen:/var/opt/mssql --name i2b2-mssql -d mcr.microsoft.com/mssql/server:2017-latest
- 
-sleep 180
-docker exec -i i2b2-data-mssql /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P '<YourStrong@Passw0rd>' -Q "backup database i2b2demodata to DISK =  N'/tmp/i2b2demodata.bak' WITH INIT , COMPRESSION;"
- 
-docker exec -i i2b2-data-mssql /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P '<YourStrong@Passw0rd>' -Q "backup database i2b2metadata to DISK =  N'/tmp/i2b2metadata.bak' WITH INIT , COMPRESSION;"
- 
-docker exec -i i2b2-data-mssql /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P '<YourStrong@Passw0rd>' -Q "backup database i2b2pm to DISK =  N'/tmp/i2b2pm.bak' WITH INIT , COMPRESSION;"
- 
-docker exec -i i2b2-data-mssql /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P '<YourStrong@Passw0rd>' -Q "backup database i2b2hive to DISK =  N'/tmp/i2b2hive.bak' WITH INIT , COMPRESSION;"
- 
-docker exec -i i2b2-data-mssql /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P '<YourStrong@Passw0rd>' -Q "backup database i2b2workdata to DISK =  N'/tmp/i2b2workdata.bak' WITH INIT , COMPRESSION;"
- 
-echo "Backup Completed"
-sleep 10
- 
- 
-docker cp i2b2-data-mssql:/tmp/i2b2demodata.bak ./i2b2demodata.bak
-docker cp i2b2-data-mssql:/tmp/i2b2metadata.bak ./i2b2metadata.bak
-docker cp i2b2-data-mssql:/tmp/i2b2pm.bak ./i2b2pm.bak
-docker cp i2b2-data-mssql:/tmp/i2b2hive.bak ./i2b2hive.bak
-docker cp i2b2-data-mssql:/tmp/i2b2workdata.bak ./i2b2workdata.bak
- 
- 
-docker cp i2b2demodata.bak i2b2-mssql:/var/opt/mssql/data/i2b2demodata.bak
-docker cp i2b2metadata.bak i2b2-mssql:/var/opt/mssql/data/i2b2metadata.bak
-docker cp i2b2pm.bak i2b2-mssql:/var/opt/mssql/data/i2b2pm.bak
-docker cp i2b2hive.bak i2b2-mssql:/var/opt/mssql/data/i2b2hive.bak
-docker cp i2b2workdata.bak i2b2-mssql:/var/opt/mssql/data/i2b2workdata.bak
- 
- 
-docker exec -i i2b2-mssql /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $username -P $password -Q  "Restore database i2b2demodata from disk = N'i2b2demodata.bak' with replace, move 'i2b2demodata' to '/var/opt/mssql/data/i2b2demodata.mdf', move 'i2b2demodata_log' to '/var/opt/mssql/data/i2b2demodata_log.ldf'"
- 
-docker exec -i i2b2-mssql /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $username -P $password -Q  "Restore database i2b2metadata from disk = N'i2b2metadata.bak' with replace, move 'i2b2metadata' to '/var/opt/mssql/data/i2b2metadata.mdf', move 'i2b2metadata_log' to '/var/opt/mssql/data/i2b2metadata_log.ldf'"
- 
- 
-docker exec -i i2b2-mssql /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $username -P $password -Q  "Restore database i2b2pm from disk = N'i2b2pm.bak' with replace, move 'i2b2pm' to '/var/opt/mssql/data/i2b2pm.mdf', move 'i2b2pm_log' to '/var/opt/mssql/data/i2b2pm_log.ldf'"
- 
- 
-docker exec -i i2b2-mssql /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $username -P $password -Q  "Restore database i2b2hive from disk = N'i2b2hive.bak' with replace, move 'i2b2hive' to '/var/opt/mssql/data/i2b2hive.mdf', move 'i2b2hive_log' to '/var/opt/mssql/data/i2b2hive_log.ldf'"
- 
- 
-docker exec -i i2b2-mssql /opt/mssql-tools/bin/sqlcmd -S $host,$port -U $username -P $password -Q  "Restore database i2b2workdata from disk = N'i2b2workdata.bak' with replace, move 'i2b2workdata' to '/var/opt/mssql/data/i2b2workdata.mdf', move 'i2b2workdata_log' to '/var/opt/mssql/data/i2b2workdata_log.ldf'"
- 
- 
-echo "Restore Completed"
-sleep 20
- 
+export SOURCE_SERVER="localhost"
+export SOURCE_USER="sa"
+export SOURCE_PASS="<YourStrong@Passw0rd>"
+export SOURCE_CRC_DB="i2b2demodata"
+export SOURCE_ONT_DB="i2b2metadata"
+export SOURCE_PM_DB="i2b2pm"
+export SOURCE_HIVE_DB="i2b2hive"
+export SOURCE_WD_DB="i2b2workdata"
+
+export TARGET_SERVER="dummy_host" #host,port
+export TARGET_PORT="1432"
+export TARGET_USER="sa"
+export TARGET_PASS="<YourStrong@Passw0rd>"
+export TARGET_CRC_DB="i2b2demodata"
+export TARGET_ONT_DB="i2b2metadata"
+export TARGET_PM_DB="i2b2pm"
+export TARGET_HIVE_DB="i2b2hive"
+export TARGET_WD_DB="i2b2workdata"
+
+if [ $TARGET_SERVER = "dummy_host" ]; then
+
+    export docker_network_gateway_ip=$(docker network inspect i2b2-net -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
+    export TARGET_SERVER=$docker_network_gateway_ip,$TARGET_PORT
+    echo $TARGET_SERVER 
+    docker run -i -e "ACCEPT_EULA=Y"  -e "SA_PASSWORD=<YourStrong@Passw0rd>"  -p 1432:1433 --name target_db_container -d mcr.microsoft.com/mssql/server:2017-latest
+
+fi 
+
+sleep 120
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Export \
+/SourceServerName:$SOURCE_SERVER \
+/SourceDatabaseName:$SOURCE_CRC_DB \
+/SourceUser:$SOURCE_USER \
+/SourcePassword:$SOURCE_PASS  \
+/TargetFile:$SOURCE_CRC_DB.bacpac
+
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Import \
+/TargetServerName:$TARGET_SERVER \
+/TargetDatabaseName:$TARGET_CRC_DB \
+/TargetUser:$TARGET_USER \
+/TargetPassword:$TARGET_PASS \
+/SourceFile:$SOURCE_CRC_DB.bacpac \
+/Diagnostics:true
+
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Export \
+/SourceServerName:$SOURCE_SERVER \
+/SourceDatabaseName:$SOURCE_ONT_DB \
+/SourceUser:$SOURCE_USER \
+/SourcePassword:$SOURCE_PASS  \
+/TargetFile:$SOURCE_ONT_DB.bacpac
+
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Import \
+/TargetServerName:$TARGET_SERVER \
+/TargetDatabaseName:$TARGET_ONT_DB \
+/TargetUser:$TARGET_USER \
+/TargetPassword:$TARGET_PASS \
+/SourceFile:$SOURCE_ONT_DB.bacpac \
+/Diagnostics:true
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Export \
+/SourceServerName:$SOURCE_SERVER \
+/SourceDatabaseName:$SOURCE_PM_DB \
+/SourceUser:$SOURCE_USER \
+/SourcePassword:$SOURCE_PASS  \
+/TargetFile:$SOURCE_PM_DB.bacpac
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Import \
+/TargetServerName:$TARGET_SERVER \
+/TargetDatabaseName:$TARGET_PM_DB \
+/TargetUser:$TARGET_USER \
+/TargetPassword:$TARGET_PASS \
+/SourceFile:$SOURCE_PM_DB.bacpac \
+/Diagnostics:true
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Export \
+/SourceServerName:$SOURCE_SERVER \
+/SourceDatabaseName:$SOURCE_HIVE_DB \
+/SourceUser:$SOURCE_USER \
+/SourcePassword:$SOURCE_PASS  \
+/TargetFile:$SOURCE_HIVE_DB.bacpac
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Import \
+/TargetServerName:$TARGET_SERVER \
+/TargetDatabaseName:TARGET_HIVE_DB \
+/TargetUser:$TARGET_USER \
+/TargetPassword:$TARGET_PASS \
+/SourceFile:$SOURCE_HIVE_DB.bacpac \
+/Diagnostics:true
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Export \
+/SourceServerName:$SOURCE_SERVER \
+/SourceDatabaseName:$SOURCE_WD_DB \
+/SourceUser:$SOURCE_USER \
+/SourcePassword:$SOURCE_PASS  \
+/TargetFile:$SOURCE_WD_DB.bacpac
+
+docker exec -i i2b2-data-mssql /opt/sqlpackage/sqlpackage \
+/Action:Import \
+/TargetServerName:$TARGET_SERVER \
+/TargetDatabaseName:TARGET_WD_DB \
+/TargetUser:$TARGET_USER \
+/TargetPassword:$TARGET_PASS \
+/SourceFile:$SOURCE_WD_DB.bacpac \
+/Diagnostics:true
+
+
+
 default_host="_IP=i2b2-data-mssql"
 default_port="_PORT=1433"
 default_username="_USER=i2b2"
@@ -83,16 +135,16 @@ default_wd_dbname="_WD_DB=i2b2workdata"
  
 #updating the .env file
 echo "Updating .env file"
-sed -i "s/${default_host}/_IP=${host}/g" .env
-sed -i "s/${default_port}/_PORT=${port}/g" .env
-sed -i "s/${default_username}/_USER=${username}/g" .env
-sed -i "s/${default_password}/_PASS=${password}/g" .env
+sed -i "s/${default_host}/_IP=${docker_network_gateway_ip}/g" .env
+sed -i "s/${default_port}/_PORT=${TARGET_PORT}/g" .env
+sed -i "s/${default_username}/_USER=${TARGET_USER}/g" .env
+sed -i "s/${default_password}/_PASS=${TARGET_PASS}/g" .env
  
-sed -i "s/${default_crc_dbname}/_CRC_DB=${crc_dbname}/g" .env
-sed -i "s/${default_ont_db_name}/_ONT_DB=${ont_dbname}/g" .env
-sed -i "s/${default_pm_db_name}/_PM_DB=${pm_dbname}/g" .env
-sed -i "s/${default_hive_dbname}/_HIVE_DB=${hive_dbname}/g" .env
-sed -i "s/${default_wd_dbname}/_WD_DB=${wd_dbname}/g" .env
+sed -i "s/${default_crc_dbname}/_CRC_DB=${TARGET_CRC_DB}/g" .env
+sed -i "s/${default_ont_db_name}/_ONT_DB=${TARGET_ONT_DB}/g" .env
+sed -i "s/${default_pm_db_name}/_PM_DB=${TARGET_PM_DB}/g" .env
+sed -i "s/${default_hive_dbname}/_HIVE_DB=${TARGET_HIVE_DB}/g" .env
+sed -i "s/${default_wd_dbname}/_WD_DB=${TARGET_WD_DB}/g" .env
  
  
 # docker rm -f i2b2-data-mssql #uncomment this line if you have space issue
